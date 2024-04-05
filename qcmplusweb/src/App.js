31b8c9a80fb94829ac930f0429e0fe82336f544f
Teclit <teklitte@gmail.com>
2024-04-05 14:52:02 +0200

    add frontend project

---
+++ qcmplusweb/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>QCMPLUS +++</h2>
+        <p>
+          C'est une nouvelle plate forme de gestion & d'évaluation des compétences
+        </p>
+        <a
+          className="App-link"
+          href="https://www.ecole-pmn.fr/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ecole-pmn
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
