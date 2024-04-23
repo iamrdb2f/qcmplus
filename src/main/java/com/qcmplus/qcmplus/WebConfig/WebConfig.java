7a98479c3fb15fcb5a305dcd3d78a3b8904718b7
Teklit TEWOLDE <teklit_tewolde@connect-tech.sncf>
2024-04-24 00:28:36 +0200

    Enabling Cross Origin Requests for a RESTful Web Service And update to jsx file

---
+++ src/main/java/com/qcmplus/qcmplus/WebConfig/WebConfig.java
@@ -0,0 +1,26 @@
+package com.qcmplus.qcmplus.WebConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")  // Allow frontend URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Optionally specify the allowed methods
+                        .allowedHeaders("*")  // Optionally specify the allowed headers
+                        .allowCredentials(true);  // Optionally specify if cookies should be allowed
+            }
+        };
+    }
+}
+
