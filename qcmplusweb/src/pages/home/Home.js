05d56913c66c80630ec0cc9402a3c98787126906
Benjamin EKIA <ekiabenjamin@gmail.com>
2024-05-30 15:15:13 +0200

    update landing page

--- qcmplusweb/src/pages/home/Home.js
+++ qcmplusweb/src/pages/home/Home.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import "./style.css";
-import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavigationMenu from "../../components/NavigationMenu/NavigationMenu";
-import QcmPlusContainer from "../../components/QcmPlusContainer/QcmPlusContainer";
+import Apropos from '../../components/apropos/Apropos';
+import Banner from '../../components/banner/Banner';
+import LeadMagnet from '../../components/leadMagnet/LeadMagnet';
+import Footer from '../../components/footer/Footer';
+import Header from '../../components/Header/Header';
+import Testimonials from '../../components/testimonials/Testimonials';
 
 const Home = () => {
     return (
         <div>
             <div className='container_header'>
-                <Container fluid className="text-center">
-                    <NavigationMenu />
-                    <QcmPlusContainer />
-                </Container>
+                <NavigationMenu />
+                <Header />
+                <Apropos />
+                <Banner />
+                <Testimonials/>
+                <LeadMagnet />
+                <Footer />
             </div>
         </div>
     )
