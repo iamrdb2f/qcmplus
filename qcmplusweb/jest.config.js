1131c82455f50073610cb2bf63afc2ebfe8414d0
Teclit <teklitte@gmail.com>
2024-08-14 00:29:28 +0200

    update React test

--- qcmplusweb/jest.config.js
+++ qcmplusweb/jest.config.js
@@ -2,9 +2,7 @@ module.exports = {
     transform: {
         '^.+\\.(js|jsx)$': 'babel-jest',
     },
-    transformIgnorePatterns: [
-        '/node_modules/(?!axios)/',
-    ],
+    transformIgnorePatterns: ['/node_modules/(?!axios)/',],
     moduleFileExtensions: ['js', 'jsx'],
     testEnvironment: 'jsdom',
 };
--- qcmplusweb/src/App.test.js
+++ qcmplusweb/src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { act } from 'react';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  act(() => {
+    render(
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+    );
+  });
+
+
+  const linkElement = screen.getByText(/commencer maintenant/i);
   expect(linkElement).toBeInTheDocument();
 });
