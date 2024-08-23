0d289d4949e9c273bd9bd1308fcbb5014c1b029e
Teclit <teklitte@gmail.com>
2024-08-24 01:31:15 +0200

    fix (front) [QCMPLUS-40] Update Dashboard component fetch data from db
    Signed-off-by: Teclit <teklitte@gmail.com>

--- qcmplusweb/src/components/Dashboard/Dashboard.jsx
+++ qcmplusweb/src/components/Dashboard/Dashboard.jsx
@@ -1,45 +1,99 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {retrieveUsers} from "../../services/UserService";
+import {getAllQuestions} from "../../services/QuestionService";
+import {retrieveQuizzes} from "../../services/QuizService";
+import {getAllExamSession} from "../../services/ExamService";
+import {getAllAnswers} from "../../services/AnswerService";
+import {ROLE} from "../../utils/UtilLists";
+import {FaBook, FaCommentAlt, FaFileAlt, FaQuestionCircle, FaUserGraduate, FaUserShield} from 'react-icons/fa';
+import './Dashboard.css';
 
 const Dashboard = () => {
+    const [adminCount, setAdminCount] = useState(0);
+    const [traineeCount, setTraineeCount] = useState(0);
+    const [questionCount, setQuestionCount] = useState(0);
+    const [quizCount, setQuizCount] = useState(0);
+    const [examCount, setExamCount] = useState(0);
+    const [answerCount, setAnswerCount] = useState(0);
+    const [errorMessage, setErrorMessage] = useState("");
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const usersResponse = await retrieveUsers();
+                const admins = usersResponse.data.filter(user => user.role && user.role.id === ROLE.ADMIN.value);
+                const trainees = usersResponse.data.filter(user => user.role && user.role.id === ROLE.USER.value);
+                setAdminCount(admins.length);
+                setTraineeCount(trainees.length);
+
+                const questionsResponse = await getAllQuestions();
+                setQuestionCount(questionsResponse.data.length);
+
+                const quizzesResponse = await retrieveQuizzes();
+                setQuizCount(quizzesResponse.data.length);
+
+                const examsResponse = await getAllExamSession();
+                setExamCount(examsResponse.data.length);
+
+                const answersResponse = await getAllAnswers();
+                setAnswerCount(answersResponse.data.length);
+            } catch (error) {
+                 setErrorMessage("Error fetching data. Please try again later.");
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <>
+            <header className="dashboard-header">
+                <h1>Dashboard</h1>
+            </header>
+            {errorMessage && <div className="error-message">{errorMessage}</div>}
             <div className="d-flex justify-content-around flex-wrap">
-            <div className="card" style={{width: "300px", marginTop:"10px", background:"#F3F8FE", border:"none"}}>
-                <div className="card-body">
-                    <h4 className="card-title">Administrateurs</h4>
-                    <p className="text-center"  style={{fontSize:"28px", fontWeight:"bold"}}>4</p>
+                <div className="card">
+                    <div className="card-body text-center">
+                        <FaUserShield size={50} className="icon" />
+                        <h4 className="card-title mt-2">Administrateurs</h4>
+                        <p className="text-center count">{adminCount}</p>
+                    </div>
                 </div>
-            </div>
-            <div className="card" style={{width: "300px", marginTop:"10px", background:"#F3F8FE", border:"none"}}>
-                <div className="card-body">
-                    <h4 className="card-title">Etudiants</h4>
-                    <p className="text-center"  style={{fontSize:"28px", fontWeight:"bold"}}>24</p>
+                <div className="card">
+                    <div className="card-body text-center">
+                        <FaUserGraduate size={50} className="icon" />
+                        <h4 className="card-title mt-2">Trainees</h4>
+                        <p className="text-center count">{traineeCount}</p>
+                    </div>
                 </div>
-            </div>
-            <div className="card" style={{width: "300px", marginTop:"10px", background:"#F3F8FE", border:"none"}}>
-                <div className="card-body">
-                    <h4 className="card-title">Questions</h4>
-                    <p className="text-center"  style={{fontSize:"28px", fontWeight:"bold"}}>14</p>
+                <div className="card">
+                    <div className="card-body text-center">
+                        <FaQuestionCircle size={50} className="icon" />
+                        <h4 className="card-title mt-2">Questions</h4>
+                        <p className="text-center count">{questionCount}</p>
+                    </div>
                 </div>
-            </div>
-            <div className="card" style={{width: "300px", marginTop:"25px", background:"#F3F8FE", border:"none"}}>
-                <div className="card-body">
-                    <h4 className="card-title">Quizs</h4>
-                    <p className="text-center"  style={{fontSize:"28px", fontWeight:"bold"}}>18</p>
+                <div className="card">
+                    <div className="card-body text-center">
+                        <FaFileAlt size={50} className="icon" />
+                        <h4 className="card-title mt-2">Quizzes</h4>
+                        <p className="text-center count">{quizCount}</p>
+                    </div>
                 </div>
-            </div>
-            <div className="card" style={{width: "300px", marginTop:"25px", background:"#F3F8FE", border:"none"}}>
-                <div className="card-body">
-                    <h4 className="card-title">Examens</h4>
-                    <p className="text-center"  style={{fontSize:"28px", fontWeight:"bold"}}>20</p>
+                <div className="card">
+                    <div className="card-body text-center">
+                        <FaBook size={50} className="icon" />
+                        <h4 className="card-title mt-2">Examens</h4>
+                        <p className="text-center count">{examCount}</p>
+                    </div>
                 </div>
-            </div>
-            <div className="card" style={{width: "300px", marginTop:"25px", background:"#F3F8FE", border:"none"}}>
-                <div className="card-body">
-                    <h4 className="card-title">Réponces</h4>
-                    <p className="text-center"  style={{fontSize:"28px", fontWeight:"bold"}}>14</p>
+                <div className="card">
+                    <div className="card-body text-center">
+                        <FaCommentAlt size={50} className="icon" />
+                        <h4 className="card-title mt-2">Réponses</h4>
+                        <p className="text-center count">{answerCount}</p>
+                    </div>
                 </div>
-            </div>
             </div>
         </>
     );
