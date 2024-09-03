ec5330fc7d34a3dc610cc37bcd3a0cee4675b408
Teclit <teklitte@gmail.com>
2024-09-03 13:00:32 +0200

    fix (Back) [MASTER] Update Question endpoint
    Signed-off-by: Teclit <teklitte@gmail.com>

--- qcmplusweb/src/services/ExamService.jsx
+++ qcmplusweb/src/services/ExamService.jsx
@@ -5,7 +5,7 @@ const EXAM_REST_API_URL = `${API_BASE_URL}/api`;
 export const getQuizzes = () => axiosInstance.get(`${EXAM_REST_API_URL}/quizzes`);
 export const getAllExamSession = () => axiosInstance.get(`${EXAM_REST_API_URL}/exam_sessions`);
 export const getAllUserExamHistory = (userId) => axiosInstance.get(`${EXAM_REST_API_URL}/exam_sessions/user/${userId}`);
-export const getQuestions = (quizId) => axiosInstance.get(`${EXAM_REST_API_URL}/quizzes/${quizId}/questions`);
+export const getQuestions = (quizId) => axiosInstance.get(`${EXAM_REST_API_URL}/questions/quizzes/${quizId}`);
 export const getAnswers = (questionId) => axiosInstance.get(`${EXAM_REST_API_URL}/questions/${questionId}/answers`);
 export const submitExamSession = (sessionData) => axiosInstance.post(`${EXAM_REST_API_URL}/exam_sessions`, sessionData);
 export const getExamSession = (sessionId) => axiosInstance.get(`${EXAM_REST_API_URL}/exam_sessions/${sessionId}`);
